11af5ae6a5d757cb8c32aa4a0306682b00284572
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-05-18 22:45:53 -0400

    Vamo al mambo !!

---
+++ RegistrosBeautyCenter/Models/Provincias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrosBeautyCenter.Models
+{
+    public class Provincias
+    {
+        [Key]
+
+        public int ProvinciaID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
