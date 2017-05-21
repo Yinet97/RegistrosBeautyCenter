a5e907321f3a1c18326a0cc218fd0d0ea871b571
robert antonio frias de leon <chico_3_13@hotmail.com>
2017-05-21 10:21:28 -0400

    provincia

--- RegistrosBeautyCenter/Models/Provincias.cs
+++ RegistrosBeautyCenter/Models/Provincias.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace RegistrosBeautyCenter.Models
 {
     public class Provincias
     {
-        [Key]
+        public IList<SelectListItem> StateNames { get; set; }
+        public IList<SelectListItem> DistrictNames { get; set; }
 
+        [Key]
         public int ProvinciaID { get; set; }
         public string Nombre { get; set; }
     }
